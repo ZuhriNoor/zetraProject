a6072326a2c6613500650fe13a847f2aa0f11ee7
Zuhri <zuhri@Laptop.com>
2023-09-17 22:25:16 +0530

    remove node modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <div>
       
+      <Header/>
       <Products/>
       
       
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import './css/LoginPage.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
