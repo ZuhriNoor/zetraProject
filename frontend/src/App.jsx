5be1eddb5331eddda1a09e86bf635c158e831c50
aswathy <aswathyudayakumar0022@gmail.com>
2023-12-02 16:52:22 +0530

    Testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,30 @@
-import React from 'react'
-import Header from './components/Header'
-import Sellform from './pages/Sellform'
-import Products from './pages/Products'
-import FaqPage from './pages/FaqPage'
-import Service from './pages/Service'
-
-
-
-
-
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Sellform from './pages/Sellform';
 import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails'; // Import the ProductDetails component
-import Home from './pages/Home'
+import FaqPage from './pages/FaqPage';
+import Service from './pages/Service';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductDetails from './pages/ProductDetails'; 
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      
-      
       <Service/>
       
-      
-  
-  )
+
       <div>
         <Navbar/>
         <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/products" element={<Products/>} /> 
         <Route path="/sell" element={<Sellform/>} />
+        <Route path="/service" element={<Service/>} />
+        <Route path="/FaqPage" element={<FaqPage/>} />
         <Route path="/product/:id" component={ProductDetails} />
         </Routes>
       </div>
-      
     </Router>
   );
 }
