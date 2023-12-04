76ff0ce3b3ab5739555dfcbd5ab74e7524ce6fd1
Zuhri <zuhri@Laptop.com>
2023-12-04 20:25:59 +0530

    correct code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import Sellform from './pages/Sellform';
-import Products from './pages/Products';
-import FaqPage from './pages/FaqPage';
-import Service from './pages/Service';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import ProductDetails from './pages/ProductDetails'; 
-import Home from './pages/Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Sellform from "./pages/Sellform";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails"; // Import the ProductDetails component
+import Home from "./pages/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Error from "./pages/Error";
+import Services from "./pages/Services";
+import Help from "./pages/Help"
+import FaqPage from "./pages/FaqPage";
+
 
 export default function App() {
   return (
-    <Router>
-      <Service/>
-      
-
       <div>
-        <Navbar/>
-        <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/products" element={<Products/>} /> 
-        <Route path="/sell" element={<Sellform/>} />
-        <Route path="/service" element={<Service/>} />
-        <Route path="/FaqPage" element={<FaqPage/>} />
-        <Route path="/product/:id" component={ProductDetails} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/sell" element={<Sellform />} />
+            <Route path="/product/:id" component={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/services" element={<Services />} />
+
+            <Route path="/help" element={<Help />} />
+            <Route path="/faq" element={<FaqPage />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/*" element={<Error />} />
+
+          </Routes>
       </div>
-    </Router>
   );
 }
