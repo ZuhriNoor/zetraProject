b4791624699b904cf29f13064b00c1ac230461b3
Zuhri <zuhri@Laptop.com>
2023-12-04 20:01:43 +0530

    Update auth, add logout func

--- src/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='bg-dark text-light p-3' style={{position: "fixed", bottom: "0px", width: "100%"}}>
-      <h6 className='text-center'>All Rights Reserved &copy; Zetra 2023</h6>
-    </div>
-  )
-}
